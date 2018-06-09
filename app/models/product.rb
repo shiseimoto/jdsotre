6ba0f707606fb681c433f156cd53b0f9869c6e4e
shichengyuan <shiseimoto@gmail.com>
2018-06-09 14:53:52 +0800

    sdk

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :title, :description, :price, :quantity, presence: true
   validates :price, numericality: { greater_than: 0}
 
+  mount_uploader :image, ImageUploader
+
 end
