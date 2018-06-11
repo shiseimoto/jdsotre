afb9e34195695f5e7665101a81bdd274d5c98153
shichengyuan <shiseimoto@gmail.com>
2018-06-11 17:11:01 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
    resources :products
+   resources :orders
  end
+
  resources :products do
    member do
      post :add_to_cart
